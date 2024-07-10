836a34cb62e37aa081e7d79bc07cf165c433581b
andres0395 <anmuce03@gmail.com>
2024-07-10 15:36:01 -0500

    basehref2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
-import { LocationStrategy, PathLocationStrategy, APP_BASE_HREF } from '@angular/common';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { NgxImageCompressService } from 'ngx-image-compress';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes,withViewTransitions()),
-    {provide:LocationStrategy,useClass:PathLocationStrategy},
-    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     NgxImageCompressService
   ]
 };
